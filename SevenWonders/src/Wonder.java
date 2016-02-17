dfed30a738f1dd34a291790bb74b3c306e2206fa
Daniel <Daniel@Daniel-PC>
2016-02-16 22:10:43 -0600

    I think I finished the wonder class.

--- SevenWonders/src/Wonder.java
+++ SevenWonders/src/Wonder.java
@@ -57,8 +57,8 @@ public class Wonder
 			case TheMausoleumOfHalicarnassusB:
 				res[CLOTH]++;
 				break;
-			case ThePyramidsOfGizaB:
 			case ThePyramidsOfGizaA:
+			case ThePyramidsOfGizaB:
 				res[STONE]++;
 				break;
 			default:
@@ -231,5 +231,298 @@ public class Wonder
 			break;
 		}
 		return cost;
+	}
+	
+	//provides a resource
+	public int[] ProvidedResources(int tier)//1 to 4
+	{
+		int [] resources = new int[NUMBER_OF_RESOURCES];
+		
+		switch(wb)
+		{
+		case TheLighthouseOfAlexandriaA:
+			if(tier == 2)
+			{
+				resources[WOOD]++;
+				resources[CLAY]++;
+				resources[ORE]++;
+				resources[STONE]++;
+			}
+			break;
+		case TheLighthouseOfAlexandriaB:
+			if(tier == 1)
+			{
+				resources[WOOD]++;
+				resources[CLAY]++;
+				resources[ORE]++;
+				resources[STONE]++;
+			}
+			if(tier == 2)
+			{
+				resources[GLASS]++;
+				resources[PAPER]++;
+				resources[CLOTH]++;
+			}
+			break;
+		default:
+			break;
+		}
+		return resources;
+	}
+	//provides a trade change.  This is always a cheaper brown trade to left and right
+	public boolean ProvidedTrade(int tier)//1 to 4
+	{
+		switch(wb)
+		{
+		case TheStatueOfZeusInOlympiaB:
+			if(tier == 1)
+				return true;
+		default:
+			return false;
+		}
+	}
+	//provides military
+	public int ProvidedMilitary(int tier)//1 to 4
+	{
+		switch(wb)
+		{
+		case TheColossusOfRhodesA:
+			if(tier == 2)
+				return 2;
+			return 0;
+		case TheColossusOfRhodesB:
+			if(tier == 1)
+				return 1;
+			if(tier == 2)
+				return 1;
+			return 0;
+		default:
+			return 0;
+		}
+	}
+	//provides science
+	public boolean ProvidedScience(int tier)//1 to 4
+	{
+		switch(wb)
+		{
+		case TheHangingGardensOfBabylonA:
+			if(tier == 2)
+				return true;
+			return false;
+		case TheHangingGardensOfBabylonB:
+			if(tier == 3)
+				return true;
+			return false;
+		default:
+			return false;
+		}
+	}
+	//provides points
+	public int ProvidedPoints(int tier)//1 to 4
+	{
+		switch(wb)
+		{
+		case TheColossusOfRhodesA:
+			if(tier == 1)
+				return 3;
+			if(tier == 3)
+				return 7;
+			break;
+		case TheColossusOfRhodesB:
+			if(tier == 1)
+				return 3;
+			if(tier == 2)
+				return 4;
+			break;
+		case TheLighthouseOfAlexandriaA:
+			if(tier == 1)
+				return 3;
+			if(tier == 3)
+				return 7;
+			break;
+		case TheLighthouseOfAlexandriaB:
+			if(tier == 3)
+				return 7;
+			break;
+		case TheTempleOfArtemisInEphesusA:
+			if(tier == 1)
+				return 3;
+			if(tier == 3)
+				return 7;
+			break;
+		case TheTempleOfArtemisInEphesusB:
+			if(tier == 1)
+				return 2;
+			if(tier == 2)
+				return 3;
+			if(tier == 3)
+				return 5;
+			break;
+		case TheHangingGardensOfBabylonA:
+			if(tier == 1)
+				return 3;
+			if(tier == 3)
+				return 7;
+			break;
+		case TheHangingGardensOfBabylonB:
+			if(tier == 1)
+				return 3;
+			break;
+		case TheStatueOfZeusInOlympiaA:
+			if(tier == 1)
+				return 3;
+			if(tier == 3)
+				return 7;
+			break;
+		case TheStatueOfZeusInOlympiaB:
+			if(tier == 2)
+				return 5;
+			break;
+		case TheMausoleumOfHalicarnassusA:
+			if(tier == 1)
+				return 3;
+			if(tier == 3)
+				return 7;
+			break;
+		case TheMausoleumOfHalicarnassusB:
+			if(tier == 1)
+				return 2;
+			if(tier == 2)
+				return 1;
+			break;
+		case ThePyramidsOfGizaA:
+			if(tier == 1)
+				return 3;
+			if(tier == 2)
+				return 5;
+			if(tier == 3)
+				return 7;
+			break;
+		case ThePyramidsOfGizaB:
+			if(tier == 1)
+				return 3;
+			if(tier == 2)
+				return 5;
+			if(tier == 3)
+				return 5;
+			if(tier == 4)
+				return 7;
+			break;
+		default:
+			break;
+		}
+		return 0;
+	}
+	//mimics a purple card
+	public boolean ProvidedPurple(int tier)//1 to 4
+	{
+		switch(wb)
+		{
+		case TheStatueOfZeusInOlympiaB:
+			if(tier == 3)
+				return true;
+		default:
+			return false;
+		}
+	}
+	//give coins when built
+	public int ProvidedCoins(int tier)//1 to 4
+	{
+		switch(wb)
+		{
+		case TheColossusOfRhodesB:
+			if(tier == 1)
+				return 3;
+			if(tier == 2)
+				return 4;
+			break;
+		case TheTempleOfArtemisInEphesusA:
+			if(tier == 2)
+				return 9;
+			break;
+		case TheTempleOfArtemisInEphesusB:
+			if(tier == 1)
+				return 4;
+			if(tier == 2)
+				return 4;
+			if(tier == 3)
+				return 4;
+			break;
+		default:
+			break;
+		}
+		return 0;
+	}
+	//play a card when built
+	public boolean ProvidedDiscardPlay(int tier)//1 to 4
+	{
+		switch(wb)
+		{
+			case TheMausoleumOfHalicarnassusA:
+				if(tier == 2)
+					return true;
+				return false;
+			case TheMausoleumOfHalicarnassusB:
+				if(tier == 1)
+					return true;
+				if(tier == 2)
+					return true;
+				if(tier == 3)
+					return true;
+				return false;
+			default:
+				return false;
+		}
+	}
+	//allow a free construction once per age
+	public boolean ProvidedFreePlay(int tier)//1 to 4
+	{
+		switch(wb)
+		{
+			case TheStatueOfZeusInOlympiaA:
+				if(tier == 2)
+					return true;
+			default:
+				return false;
+		}
+	}
+	//allow construction of card 7 once per age
+	public boolean ProvidedSevenPlay(int tier)//1 to 4
+	{
+		switch(wb)
+		{
+			case TheHangingGardensOfBabylonB:
+				if(tier == 2)
+					return true;
+			default:
+				return false;
+		}
+	}
+	public double[] LeftPointsOfWonderCards()
+	{
+		double[] points = new double[NumberOfWonderTiers()];
+		if(NumberOfWonderTiers() == 2)
+		{
+			points[1] = (0.61+2.93)/9.81;
+			points[2] = (0.61+2.93+2.90)/9.81;
+		}
+		if(NumberOfWonderTiers() == 3)
+		{
+			points[1] = (0.61)/9.81;
+			points[2] = (0.61+2.93)/9.81;
+			points[3] = (0.61+2.93+2.90)/9.81;
+		}
+		if(NumberOfWonderTiers() == 4)
+		{
+			points[1] = -0.30/9.81;
+			points[2] = 2.35/9.81;
+			points[3] = (3.81+1.15)/9.81;
+			points[4] = (3.81+3.75)/9.81;
+		}
+		return points;
+	}
+	public double WidthOfWonderCards()
+	{
+		return 2.555/9.81;
 	}
 }
